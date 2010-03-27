200127058addada7e885adc87404a7a44275afab
Esa-Matti Suuronen <esa-matti@suuronen.org>
2010-03-27 18:33:46 +0200

    again setup apps.vcs

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
       author_email='esa-matti@suuronen.org',
       url='http://github.com/epeli/subssh',
       license='GPL',
-      packages=['subssh', 'subssh.apps'],
+      packages=['subssh', 'subssh.apps', 'subssh.apps.vcs'],
       package_dir={'subssh': 'subssh'},
       package_data={'subssh': ['default/*']},
       data_files=[('share/subssh/doc', docs)],
