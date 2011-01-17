d93ff973c63d7ad6275a5e297907e84c5f61627a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-01-17 11:16:37 +0200

    Use setuptools instead of distutils so that we can use script generation when using buildout.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Created on Mar 27, 2010
 '''
 
 import os
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 
 docs = [os.path.join("doc", dir) for dir in os.listdir("doc")]
@@ -17,20 +17,17 @@ f.close()
 
 
 setup(
-      name='subssh',
-      version=version,
-      description='Bare minimal shell for using eg. Git and Subversion over SSH',
-      author='Esa-Matti Suuronen',
-      author_email='esa-matti@suuronen.org',
-      url='http://github.com/epeli/subssh',
-      license='AGPL',
-      package_dir={'subssh': 'subssh'},
-      package_data={'subssh': ['default/*', 'version.txt']},
-      data_files=[('share/subssh/doc', docs)],
-      packages=['subssh', 
-                'subssh.app', 
-                'subssh.app.vcs'],
-      scripts=['scripts/subssh', 
-               'scripts/subssh-admin', 
-               'scripts/subssh-xmlrpc'],
+    name='subssh',
+    version=version,
+    description='Bare minimal shell for using eg. Git and Subversion over SSH',
+    author='Esa-Matti Suuronen',
+    author_email='esa-matti@suuronen.org',
+    url='http://github.com/epeli/subssh',
+    license='AGPL',
+    package_dir={'subssh': 'subssh'},
+    include_package_data = True,
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': "subssh=subssh.dispatcher:dispatch",
+    },
 )
